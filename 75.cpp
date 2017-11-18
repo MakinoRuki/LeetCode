e99aba1363142dc36f91b7e958349c8fa37ed0ce
MakinoRuki <muye67@gmail.com>
2017-11-17 23:27:58 -0800

    Update 75.cpp

--- 75.cpp
+++ 75.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    void sortColors(int A[], int n) {
-        if(n==0)return ;
-        int lft=-1;
-        int rit=n;
-        for(int i=0;i<min(n,rit);){
-            if(A[i]==0){
-                if(i==lft+1){
-                    lft++;
-                    i++;
-                }else{
-                    swap(A[lft+1],A[i]);
-                    lft++;
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        for (int i = 0; i < n && i <= r; ++i) {
+            if (l >= r) break;
+            if (nums[i] == 0) {
+                if (i != l) {
+                    swap(nums[i], nums[l]);
+                    i--;
                 }
-            }else if(A[i]==2){
-                if(i==rit-1){
-                    rit--;
-                    i++;
-                }else{
-                    swap(A[rit-1],A[i]);
-                    rit--;
+                l++;
+            } else if (nums[i] == 2) {
+                if (i != r) {
+                    swap(nums[i], nums[r]);
+                    i--;
                 }
-            }else i++;
+                r--;
+            }
         }
     }
 };
