819b2fd8ae17ea6270c34fbeb7b330f2adf3f368
MakinoRuki <muye67@gmail.com>
2018-08-12 13:46:54 -0700

    Create lc891.cpp

---
+++ lc891.cpp
@@ -0,0 +1,20 @@
+/*dp[M,K]=M move & K egg, max floors we can check*/
+/*egg break or not, we get either part, plus current checked floor*/
+class Solution {
+public:
+    int dp[2][102];
+    int superEggDrop(int K, int N) {
+        memset(dp, 0, sizeof(dp));
+        int cnt = 0;
+        while(true) {
+            ++cnt;
+            int now = cnt & 1;
+            memset(dp[now], 0, sizeof(dp[now]));
+            for (int j = 1; j <= K; ++j) {
+                dp[now][j] = dp[now ^ 1][j] + dp[now ^ 1][j - 1] + 1;
+                if (dp[now][j] >= N) return cnt;
+            }
+        }
+        return 0;
+    }
+};
