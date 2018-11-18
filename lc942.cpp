5a6abcb4f6cbc8c55e4e8e0b8c52eb0cd0d774fd
MakinoRuki <muye67@gmail.com>
2018-11-18 12:25:53 -0800

    Create lc942.cpp

---
+++ lc942.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string S) {
+        int n = S.size();
+        vector<int> tmp;
+        tmp.clear();
+        for (int i = 0; i <= n; ++i) {
+            tmp.push_back(i);
+        }
+        vector<int> ans;
+        ans.clear();
+        for (int i = 0; i < n; ++i) {
+           if (S[i] == 'D') {
+               int j;
+               for (j = i; j < n; ++j) {
+                   if (S[j] != 'D') break;
+               }
+               for (int k = j; k >= i; --k) {
+                   ans.push_back(tmp[k]);
+               }
+               i = j;
+           }  else {
+               ans.push_back(tmp[i]);
+           }
+        }
+        if (ans.size() < tmp.size()) ans.push_back(tmp.back());
+        return ans;
+    }
+};
