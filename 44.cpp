df50f28559086203c03e089f06850bc8fa421b8e
MakinoRuki <muye67@gmail.com>
2019-07-23 21:53:11 -0700

    Update 44.cpp

--- 44.cpp
+++ 44.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
+    int n, m;
+    int star, stari;
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
+        n = s.size();
+        m = p.size();
+        star = -1;
+        stari = -1;
         int i = 0, j = 0;
-        int star = -1, starCover = -1;
         while(i < n) {
-            if (s[i] == p[j]) {
+            if (s[i] == p[j] || p[j] == '?') {
                 i++;
                 j++;
             } else {
-                if (p[j] == '?') {
-                    i++;
+                if (p[j] == '*') {
+                    star = j;
+                    stari = i;
                     j++;
                 } else {
-                    if (p[j] == '*') {
-                        star = j;
-                        starCover = i;
-                        j++;
-                    } else {
-                        if (star < 0) return false;
-                        j = star + 1;
-                        i = starCover + 1;
-                        while(i < n && (s[i] != p[j] && p[j] != '?')) {
-                            i++;
-                        }
-                        starCover = i;
-                    }
+                    if (star < 0) return false;
+                    i = stari + 1;
+                    j = star + 1;
+                    stari = i;
                 }
             }
         }
-        while(j < m && p[j] == '*') j++;
-        if (i == n && j == m) return true;
-        return false;
+        while(j < m) {
+            if (p[j] != '*') return false;
+            j++;
+        }
+        return true;
     }
 };
