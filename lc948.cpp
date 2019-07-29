3bfc86375ed1d6c12510243e46ae2f5239a45f30
MakinoRuki <muye67@gmail.com>
2019-07-28 23:54:16 -0700

    Update lc948.cpp

--- lc948.cpp
+++ lc948.cpp
@@ -1,1 +1,32 @@
-把token排序然后能换成score就换成score。power不够了就用score换最大的token的power。
+// 把token排序然后能换成score就换成score。power不够了就用score换最大的token的power。
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int P) {
+        int n = tokens.size();
+        int pts = 0;
+        int r = n-1;
+        int ans = 0;
+        sort(tokens.begin(), tokens.end());
+        for (int i = 0; i <= r; ++i) {
+            if (P >= tokens[i]) {
+                P -= tokens[i];
+                pts++;
+            } else {
+                while(pts > 0 && r > i) {
+                    pts--;
+                    P += tokens[r--];
+                    if (P >= tokens[i]) break;
+                }
+                if (r > i && P >= tokens[i]) {
+                    P -= tokens[i];
+                    pts++;
+                } else {
+                    break;
+                }
+            }
+            ans = max(ans, pts);
+        }
+        ans = max(ans, pts);
+        return ans;
+    }
+};
