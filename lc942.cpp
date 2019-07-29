34451207d62a8f24562018c9abad44b1a9e85a58
MakinoRuki <muye67@gmail.com>
2019-07-28 23:16:35 -0700

    Update lc942.cpp

--- lc942.cpp
+++ lc942.cpp
@@ -1,3 +1,4 @@
+/* Solution 1: */
 class Solution {
 public:
     vector<int> diStringMatch(string S) {
@@ -27,3 +28,24 @@ public:
         return ans;
     }
 };
+
+/* Solution 2: */
+/* class Solution {
+public:
+    vector<int> diStringMatch(string S) {
+        int n = S.size();
+        vector<int> ans;
+        ans.push_back(0);
+        for (int i = 0; i < n; ++i) {
+            if (S[i] == 'I') ans.push_back(i+1);
+            else {
+                int j;
+                for (j = i; j >= 0; --j) {
+                    if (S[j] == 'I') break;
+                }
+                ans.insert(ans.begin()+j+1, i+1);
+            }
+        }
+        return ans;
+    }
+}; */
