00b567fa4c21dd88e2bcd60921c3d0a080528a21
MakinoRuki <muye67@gmail.com>
2019-09-02 14:16:49 -0700

    Update 581.cpp

--- 581.cpp
+++ 581.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> maxn;
-        vector<int> minn;
         int n = nums.size();
-        for (int i = 0; i < n; ++i) {
-            if (i == 0) maxn.push_back(nums[i]);
-            else maxn.push_back(max(maxn.back(), nums[i]));
-        }
+        int minv = INT_MAX;
+        int l=n, r=-1;
         for (int i = n - 1; i >= 0; --i) {
-            if (i == n - 1) minn.push_back(nums[i]);
-            else minn.push_back(min(minn.back(), nums[i]));
+            if (nums[i] > minv) {
+                l = i;
+            }
+            minv = min(minv, nums[i]);
         }
-        reverse(minn.begin(), minn.end());
-        int l, r;
-        for (l = 0; l < n; ++l) {
-            if (l > 0 && nums[l] < nums[l - 1]) break;
-            if (l < n - 1 && nums[l] > minn[l + 1]) break;
+        int maxv = INT_MIN;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] < maxv) {
+                r = i;
+            }
+            maxv = max(maxv, nums[i]);
         }
-        for (r = n - 1; r >= 0; --r) {
-            if (r < n - 1 && nums[r] > nums[r + 1]) break;
-            if (r > 0 && nums[r] < maxn[r - 1]) break;
+        if (l < n) {
+            return r-l+1;
         }
-        return max(r - l + 1, 0);
+        return 0;
     }
 };
