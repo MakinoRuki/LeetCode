07cef50315584738e0c1fc03568fe33644a00080
MakinoRuki <muye67@gmail.com>
2019-09-28 21:55:50 -0700

    Update 146.cpp

--- 146.cpp
+++ 146.cpp
@@ -1,78 +1,39 @@
-// O(1) 用链表。
-
-struct Node {
-  int key;
-  int val;
-  Node* pre;
-  Node* nxt;
-  Node(int k, int v) : key(k), val(v), pre(NULL), nxt(NULL) {}
-};
+// O(1) 用list。
 
 class LRUCache {
-  public:
-  int cap_;
-  int sz_;
-  Node* head;
-  Node* tail;
-  unordered_map<int, Node*> mm;
-  LRUCache(int capacity) {
-    cap_ = capacity;
-    sz_ = 0;
-    head= NULL;
-    tail= NULL;
-  }
-  void modifyCache(int key, int val) {
-    Node* cur = mm[key];
-    if (cur == head) {
-      if (!head->nxt) {
-          head->val=val;
-          return;
-      }
-      head=head->nxt;
-      head->pre=NULL;
-      tail->nxt=cur;
-      cur->pre=tail;
-      cur->nxt=NULL;
-      tail = cur;
-    } else if (cur!=tail) {
-      Node* p1 = cur->pre;
-      Node* p2 = cur->nxt;
-      p1->nxt=p2;
-      p2->pre=p1;
-      tail->nxt=cur;
-      cur->nxt=NULL;
-      cur->pre=tail;
-      tail=cur;
+public:
+    list<pair<int, int>> cache;
+    unordered_map<int, list<pair<int, int>>::iterator> mm;
+    int cap;
+    LRUCache(int capacity) {
+        cap = capacity;
     }
-    cur->val = val;
-  }
-  void put(int key, int val) {
-    if (mm.find(key) != mm.end()) {
-      modifyCache(key, val);
-    } else {
-      if (sz_ >= cap_) {
-        mm.erase(head->key);
-        head = head->nxt;
-        head->pre = NULL;
-        sz_--;
-      }
-      Node* cur = new Node(key, val);
-      if (head == NULL) {
-          head = cur;
-          tail = cur;
-      } else {
-          tail->nxt=cur;
-          cur->pre=tail;
-          cur->nxt=NULL;
-          tail=cur;
-      }
-      mm[key] = cur;
-      sz_++;
+    void put(int key, int val) {
+        if (mm.find(key) != mm.end()) {
+            auto itr = mm[key];
+            cache.erase(itr);
+        }
+        cache.push_front(make_pair(key, val));
+        mm[key] = cache.begin();
+        auto t = mm[key];
+        if (cache.size() > cap) {
+            mm.erase(cache.back().first);
+            cache.pop_back();
+        }
     }
-  }
-  int get(int key) {
-    if (mm.find(key) == mm.end()) return -1;
-    modifyCache(key, mm[key]->val);
-    return mm[key]->val;
-  }
+    int get(int key) {
+        if (mm.find(key) == mm.end()) return -1;
+        auto tmp = *mm[key];
+        cache.erase(mm[key]);
+        cache.push_front(tmp);
+        mm[key] = cache.begin();
+        return tmp.second;
+    }
 };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
